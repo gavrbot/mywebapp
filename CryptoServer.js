6898af570b40ca56dea1be65be22cd71c7f41a92
gavrbot <asakurazik2@gmail.com>
2022-05-21 21:48:58 +0400

    moved cryptographic part to backend service, also endpoint for checking password also has been added to the service

--- CryptoServer.js
+++ CryptoServer.js
@@ -1,18 +1,105 @@
+const JSEncrypt = require('nodejs-jsencrypt').default;
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
-const axios = require('axios')
 require('dotenv').config()
+
+const createOneTimePassword = () => {
+    var result = [];
+    var characters = '0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 6; i++ ) {
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');
+}
+
+var genPass = createOneTimePassword()
 
 const app = express()
 
+// la problema
+//const publicCheckKey = process.env.PUBLIC_KEY
+const privateCheckKey = process.env.PRIVATE_KEY
+const publicCheckKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBO71iVLEu7umehJ0HJ5501wW1rSKTL3hkng+WRJZCnQ/3ZWLJrdLdgRRkaQMpzdF+AmqvtioluXjZdyrhLpkRtcAkjgQbBnRnL5zirJydmYZJU8CRSjrrER439hHTD9Zml1y9Pa//NPcfnd9iw6kZSX5rArEzFiKp3hRZGgecYwIDAQAB"
+// const privateCheckKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAME7vWJUsS7u6Z6EnQcnnnTXBbWtIpMveGSeD5ZElkKdD/dlYsmt0t2BFGRpAynN0X4Caq+2KiW5eNl3KuEumRG1wCSOBBsGdGcvnOKsnJ2ZhklTwJFKOusRHjf2EdMP1maXXL09r/809x+d32LDqRlJfmsCsTMWIqneFFkaB5xjAgMBAAECgYEAm4K/hI5SVkoyO7/QPDzXWoLd9ntTEw8mHhvSwYWLRCrw+ZJfsZ2x0VAboD+fKxqYGYhKYgUB4IBm0OUF3lnJF0CmzWYcPg7QpsNRU2iCp50c6EyGmNItpPQycnTx68xG1RTYE1EXfwAmHDeB9Bbsk87HHdJQqjANnUFeSDPq9/ECQQDelkKO7rZA/KNKmQJZIqGEGWvlMb+5SuHCiVRLT3vqKuaub0Fym1Ey6ngVYN5yZt2tnUV6brfwr+/y3TyQlq0pAkEA3j11Ju32DsAzC4dtmDM4vee8KY7OpnE2dkEGA9K6U8M/R3y3WQEtUC8kqf+m9EXOdiMlB72Ld0N0TojQ+R6iqwJAMcDShdJz6JjQAyeqb7Qe+EEabfOt0EQdrHc34VGV+CS4xXrW3UA8aS4hw12Qu2+k017ZHeHLucAJ2XZ8SDF16QJAE+woe2Proeji6o6qaXF2Dbgfaw5NQih1/GXZ1y/l2ipvmsX4Xbc4S67eN4seeVlkp7yAzk/Ul81pOL0VFrADXwJBAI/2Oq2AcSNOu6QY3JuzU4kN1mjKGDkBqmV3nHev9bp7NLyoasqzg8xo9lvuYjPpo47JXPgpH+CXXkLTTmqk/m8=";
+app.use(express.json())
 app.use(cors())
 
-app.get('/keys', (req,res) => {
-    res.json('{' +
-        '  "public": "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBO71iVLEu7umehJ0HJ5501wW1rSKTL3hkng+WRJZCnQ/3ZWLJrdLdgRRkaQMpzdF+AmqvtioluXjZdyrhLpkRtcAkjgQbBnRnL5zirJydmYZJU8CRSjrrER439hHTD9Zml1y9Pa//NPcfnd9iw6kZSX5rArEzFiKp3hRZGgecYwIDAQAB",' +
-        '  "private": "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAME7vWJUsS7u6Z6EnQcnnnTXBbWtIpMveGSeD5ZElkKdD/dlYsmt0t2BFGRpAynN0X4Caq+2KiW5eNl3KuEumRG1wCSOBBsGdGcvnOKsnJ2ZhklTwJFKOusRHjf2EdMP1maXXL09r/809x+d32LDqRlJfmsCsTMWIqneFFkaB5xjAgMBAAECgYEAm4K/hI5SVkoyO7/QPDzXWoLd9ntTEw8mHhvSwYWLRCrw+ZJfsZ2x0VAboD+fKxqYGYhKYgUB4IBm0OUF3lnJF0CmzWYcPg7QpsNRU2iCp50c6EyGmNItpPQycnTx68xG1RTYE1EXfwAmHDeB9Bbsk87HHdJQqjANnUFeSDPq9/ECQQDelkKO7rZA/KNKmQJZIqGEGWvlMb+5SuHCiVRLT3vqKuaub0Fym1Ey6ngVYN5yZt2tnUV6brfwr+/y3TyQlq0pAkEA3j11Ju32DsAzC4dtmDM4vee8KY7OpnE2dkEGA9K6U8M/R3y3WQEtUC8kqf+m9EXOdiMlB72Ld0N0TojQ+R6iqwJAMcDShdJz6JjQAyeqb7Qe+EEabfOt0EQdrHc34VGV+CS4xXrW3UA8aS4hw12Qu2+k017ZHeHLucAJ2XZ8SDF16QJAE+woe2Proeji6o6qaXF2Dbgfaw5NQih1/GXZ1y/l2ipvmsX4Xbc4S67eN4seeVlkp7yAzk/Ul81pOL0VFrADXwJBAI/2Oq2AcSNOu6QY3JuzU4kN1mjKGDkBqmV3nHev9bp7NLyoasqzg8xo9lvuYjPpo47JXPgpH+CXXkLTTmqk/m8="' +
-        '}')
+app.post('/getQRCodeData', (req,res) => {
+    let message = createMessage(req.body.amount, req.body.cardNumber)
+    res.json({qrCodeData: message})
+})
+
+app.post('/checkPass', (req,res) => {
+    let message = checkPassword(req.body.pass)
+    res.json({result: message})
 })
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+function createMessage(amount, cardNumber) {
+    console.log(privateCheckKey)
+    console.log(publicCheckKey)
+    genPass = createOneTimePassword()
+
+    const CryptoJS = require("crypto-js");
+
+    let operationType = getOperationType()
+
+    const encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicCheckKey)
+
+    const encryptedPass = encrypt.encrypt(genPass);
+
+    const timeStamp = getTimeStamp()
+
+    var message = cardNumber.slice(0,4)+ cardNumber.slice(-4)+
+        " "+timeStamp+
+        " "+amount+
+        " "+operationType+
+        " "+encryptedPass;
+
+    var sign = new JSEncrypt();
+    sign.setPrivateKey(privateCheckKey);
+
+    var signature = sign.sign(message, CryptoJS.SHA256, "sha256");
+
+    const qrCodeMessage = message + " " + signature
+
+    return qrCodeMessage
+}
+
+function checkPassword(pass) {
+    return pass === genPass
+}
+
+
+function getTimeStamp() {
+    const currentDate = new Date();
+    const date = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
+    const time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+    return  date + "(" + time + ")";
+}
+
+function getOperationType() {
+    switch (getRandomInt(1,5)) {
+        case 1:
+            return "bank_transfer"
+        case 2:
+            return "P2P_transfer"
+        case 3:
+            return "system_transfer"
+        case 4:
+            return "shop_purchase"
+        default:
+            return "bank_transfer"
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
--- src/components/CreditCardForm.js
+++ src/components/CreditCardForm.js
@@ -6,18 +6,14 @@ import "./CreditCardForm.css";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import QRCode from "qrcode.react";
-import {JSEncrypt} from "jsencrypt";
+import axios from "axios";
 
-const publicCheckKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBO71iVLEu7umehJ0HJ5501wW1rSKTL3hkng+WRJZCnQ/3ZWLJrdLdgRRkaQMpzdF+AmqvtioluXjZdyrhLpkRtcAkjgQbBnRnL5zirJydmYZJU8CRSjrrER439hHTD9Zml1y9Pa//NPcfnd9iw6kZSX5rArEzFiKp3hRZGgecYwIDAQAB"
-
-const privateCheckKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAME7vWJUsS7u6Z6EnQcnnnTXBbWtIpMveGSeD5ZElkKdD/dlYsmt0t2BFGRpAynN0X4Caq+2KiW5eNl3KuEumRG1wCSOBBsGdGcvnOKsnJ2ZhklTwJFKOusRHjf2EdMP1maXXL09r/809x+d32LDqRlJfmsCsTMWIqneFFkaB5xjAgMBAAECgYEAm4K/hI5SVkoyO7/QPDzXWoLd9ntTEw8mHhvSwYWLRCrw+ZJfsZ2x0VAboD+fKxqYGYhKYgUB4IBm0OUF3lnJF0CmzWYcPg7QpsNRU2iCp50c6EyGmNItpPQycnTx68xG1RTYE1EXfwAmHDeB9Bbsk87HHdJQqjANnUFeSDPq9/ECQQDelkKO7rZA/KNKmQJZIqGEGWvlMb+5SuHCiVRLT3vqKuaub0Fym1Ey6ngVYN5yZt2tnUV6brfwr+/y3TyQlq0pAkEA3j11Ju32DsAzC4dtmDM4vee8KY7OpnE2dkEGA9K6U8M/R3y3WQEtUC8kqf+m9EXOdiMlB72Ld0N0TojQ+R6iqwJAMcDShdJz6JjQAyeqb7Qe+EEabfOt0EQdrHc34VGV+CS4xXrW3UA8aS4hw12Qu2+k017ZHeHLucAJ2XZ8SDF16QJAE+woe2Proeji6o6qaXF2Dbgfaw5NQih1/GXZ1y/l2ipvmsX4Xbc4S67eN4seeVlkp7yAzk/Ul81pOL0VFrADXwJBAI/2Oq2AcSNOu6QY3JuzU4kN1mjKGDkBqmV3nHev9bp7NLyoasqzg8xo9lvuYjPpo47JXPgpH+CXXkLTTmqk/m8=";
+const INITIAL_COUNT = 60
 
 const STATUS = {
     STARTED: 'Started',
     STOPPED: 'Stopped',
 }
-
-const INITIAL_COUNT = 60
 
 const CreditCardForm = () => {
     const { handleChange, handleFocus, handleSubmit, values, errors } = useForm();
@@ -33,26 +29,28 @@ const CreditCardForm = () => {
     const minutesToDisplay = minutesRemaining % 60
 
 
-    const onHandleSubmit = useCallback(() => {
+    const onHandleSubmit = useCallback(async () => {
         setGenPass(createOneTimePassword(6))
 
-        const qrCodeMessage = createMessage(values, genPass)
+        const qrCodeMessageFromServer = await getQRCodeMessageFromServer(values)
 
-        setQrCodeValue(qrCodeMessage)
+        console.log("qrCodeData: " + qrCodeMessageFromServer)
+
+        setQrCodeValue(qrCodeMessageFromServer)
 
         setShow(true)
     }, [values, genPass])
 
-    const onConfirmPassword = useCallback(() => {
-        console.log(genPass)
-        console.log(pass)
-        if(pass === genPass){
+    const onConfirmPassword = useCallback(async () => {
+
+        const isPasswordCorrect = await comparePasswords(pass)
+        if(isPasswordCorrect){
             console.log("Success")
             alert("success")
         }
         else
             console.log("Password is not correct")
-            alert("error")
+        alert("error")
     }, [pass,genPass, qrCodeValue])
 
     const handleStart = () => {
@@ -211,41 +209,39 @@ const createOneTimePassword = () => {
 
 const twoDigits = (num) => String(num).padStart(2, '0')
 
-function createMessage(values, genPass) {
-    const CryptoJS = require("crypto-js");
+async function getQRCodeMessageFromServer(values) {
+    let cardNumber = values.cardNumber.slice(0,4)+ values.cardNumber.slice(-4)
 
-    let operationType = getOperationType()
+    const options = {
+        method: 'POST',
+        url: 'http://localhost:8000/getQRCodeData',
+        data: {
+            amount: values.cardAmount,
+            cardNumber: cardNumber
+        }
+    }
 
-    const encrypt = new JSEncrypt();
-    encrypt.setPublicKey(publicCheckKey)
-
-    const encryptedPass = encrypt.encrypt(genPass);
-
-    const timeStamp = getTimeStamp()
-
-    var message = values.cardName.slice(0,values.cardName.indexOf(" "))+
-        " "+values.cardNumber.slice(0,4)+ values.cardNumber.slice(-4)+
-        " "+timeStamp+
-        " "+values.cardAmount+
-        " "+operationType+
-        " "+encryptedPass;
-
-    var sign = new JSEncrypt();
-    sign.setPrivateKey(privateCheckKey);
-
-    var signature = sign.sign(message, CryptoJS.SHA256, "sha256");
-
-    const qrCodeMessage = message + " " + signature
-
-    return qrCodeMessage
+    try {
+        const {data} = await axios.request(options)
+        return data.qrCodeData
+    } catch (e) {
+    }
 }
 
-function getTimeStamp() {
-    const currentDate = new Date();
-    const date = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
-    const time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+async function comparePasswords(pass) {
+    const options = {
+        method: 'POST',
+        url: 'http://localhost:8000/checkPass',
+        data: {
+            pass: pass
+        }
+    }
 
-    return  date + "(" + time + ")";
+    try {
+        const {data} = await axios.request(options)
+        return data.result
+    } catch (e) {
+    }
 }
 
 function useInterval(callback, delay) {
@@ -264,27 +260,6 @@ function useInterval(callback, delay) {
             return () => clearInterval(id)
         }
     }, [delay])
-}
-
-function getOperationType() {
-    switch (getRandomInt(1,5)) {
-        case 1:
-            return "bank_transfer"
-        case 2:
-            return "P2P_transfer"
-        case 3:
-            return "system_transfer"
-        case 4:
-            return "shop_purchase"
-        default:
-            return "bank_transfer"
-    }
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export default CreditCardForm;
